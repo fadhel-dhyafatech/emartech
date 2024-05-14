31236c45fe57798eb028273ddf74899c87160cb1
devbilalashraf <devbilal.ashraf786@gmail.com>
2024-05-15 03:12:24 +0500

    Drawer fixes

--- client/src/components/common/sideBar.tsx
+++ client/src/components/common/sideBar.tsx
@@ -1,93 +1,3 @@
-// import { Box, List, styled } from "@mui/material";
-// import { FC } from "react";
-// import { SideBarBtns } from "./sideBarNavLinks";
-// import { Logo } from "@/components/Logo";
-
-// const SideBarWrapper = styled(Box)`
-//   position: fixed;
-//   top: 0;
-//   left: 0;
-//   width: 250px;
-//   height: 100vh;
-//   display: block;
-//   z-index: 2;
-//   transition: transform 0.35 ease-in-out;
-// `;
-
-// const SideBarBody = styled(Box)`
-//   background: linear-gradient(0deg, #3558f4, #1d8cf8);
-//   height: 100vh;
-//   overflow: hidden;
-// `;
-
-// const ParentList = styled(List)`
-//   position: relative;
-//   list-style: none;
-//   padding: 0;
-//   display: block;
-//   transition: all 0.5s ease;
-// `;
-
-// const dummyList = [
-//   {
-//     to: "/dashboard/home",
-//     icon: <i className="fa-solid fa-id-card"></i>,
-//     title: "Dashboard",
-//   },
-//   {
-//     to: "/inventory",
-//     icon: <i className="fa-solid fa-id-card"></i>,
-//     title: "Inventory",
-//     subBtn: ["All Inventory", "Shipments", "Analytics"],
-//   },
-//   {
-//     to: "/catalog",
-//     icon: <i className="fa-solid fa-id-card"></i>,
-//     title: "Catalog",
-//     subBtn: ["Add Product", "Selling Application"],
-//   },
-//   {
-//     to: "/orders",
-//     icon: <i className="fa-solid fa-id-card"></i>,
-//     title: "Orders",
-//     subBtn: ["Manage Orders", "Manage Returns", "Manage Claim"],
-//   },
-
-//   {
-//     to: "/shipments",
-//     icon: <i className="fa-solid fa-id-card"></i>,
-//     title: "Shipments",
-//   },
-//   {
-//     to: "/performance",
-//     icon: <i className="fa-solid fa-id-card"></i>,
-//     title: "Performance",
-//     subBtn: ["Store Analytic", "Store Review"],
-//   },
-//   {
-//     to: "/payments",
-//     icon: <i className="fa-solid fa-id-card"></i>,
-//     title: "Payments",
-//   },
-// ];
-
-// export const SideBar: FC = () => {
-//   return (
-//     <div>
-//       <SideBarWrapper>
-//         <SideBarBody>
-//           <Logo />
-//           <ParentList>
-//             {dummyList.map((item, index) => (
-//               <SideBarBtns to={item.to} icon={item.icon} title={item.title} />
-//             ))}
-//           </ParentList>
-//         </SideBarBody>
-//       </SideBarWrapper>
-//     </div>
-//   );
-// };
-
 // components/Sidebar.js
 import Box from "@mui/material/Box";
 import MuiDrawer from "@mui/material/Drawer";
@@ -104,15 +14,15 @@ import IconButton from "@mui/material/IconButton";
 
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import Collapse from "@mui/material/Collapse";
-import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import { ListItemButton } from "@mui/material";
+import { Dispatch, SetStateAction } from "react";
 
-const drawerWidth = 240;
+export const drawerWidth = 240;
 
 const openedMixin = (theme: Theme): CSSObject => ({
   width: drawerWidth,
@@ -140,40 +50,24 @@ const DrawerHeader = styled("div")(({ theme }) => ({
   alignItems: "center",
   justifyContent: "flex-end",
   padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
   ...theme.mixins.toolbar,
 }));
 
-// interface AppBarProps extends MuiAppBarProps {
-//   open?: boolean;
-// }
+
 
 interface SidebarProps {
   dummyList: {
-    to: string;
-    icon: any;
+    to?: string;
+    icon?: any;
     title: string;
-    subBtn?: string[];
+    subBtn?: {
+      to: string,
+      title: string,
+    }[];
   }[];
+  isDrawerOpen: boolean,
+  setIsDrawerOpen: Dispatch<SetStateAction<boolean>>
 }
-
-// const AppBar = styled(MuiAppBar, {
-//   shouldForwardProp: (prop) => prop !== "open",
-// })<AppBarProps>(({ theme, open }) => ({
-//   zIndex: theme.zIndex.drawer + 1,
-//   transition: theme.transitions.create(["width", "margin"], {
-//     easing: theme.transitions.easing.sharp,
-//     duration: theme.transitions.duration.leavingScreen,
-//   }),
-//   ...(open && {
-//     marginLeft: drawerWidth,
-//     width: `calc(100% - ${drawerWidth}px)`,
-//     transition: theme.transitions.create(["width", "margin"], {
-//       easing: theme.transitions.easing.sharp,
-//       duration: theme.transitions.duration.enteringScreen,
-//     }),
-//   }),
-// }));
 
 const Drawer = styled(MuiDrawer, {
   shouldForwardProp: (prop) => prop !== "open",
@@ -192,17 +86,10 @@ const Drawer = styled(MuiDrawer, {
   }),
 }));
 
-export const Sidebar = ({ dummyList }: SidebarProps) => {
-  console.log("DUMMYLIST", dummyList);
-  //   const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
-
-  //   const handleDrawerOpen = () => {
-  //     setOpen(true);
-  //   };
-
+export const Sidebar = ({ dummyList, isDrawerOpen, setIsDrawerOpen }: SidebarProps) => {
+ 
   const handleDrawerClose = () => {
-    setOpen(!open);
+    setIsDrawerOpen(!isDrawerOpen)
   };
 
   const [openSubmenu, setOpenSubmenu] = React.useState<string>("");
@@ -215,21 +102,21 @@ export const Sidebar = ({ dummyList }: SidebarProps) => {
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
 
-      <Drawer variant="permanent" open={open}>
+      <Drawer variant="permanent" open={isDrawerOpen}>
         <Box sx={{
           height: "100%", background: "linear-gradient(270.59deg, #079759 0.48%, #35C350 99.47%)"
         }}>
           <DrawerHeader>
-            {open && (
+            {isDrawerOpen && (
               <img
-                src={`../../src/assets/images/dashboard-white-logo.svg`}
+                src={"../../src/assets/images/dashboard-white-logo.svg"}
                 alt="logo"
               // style={{ width: "120px" }}
               />
             )}
 
             <IconButton onClick={handleDrawerClose}>
-              {open ? (
+              {isDrawerOpen ? (
                 <ChevronRightIcon sx={{ color: "white" }} />
               ) : (
                 <MenuIcon sx={{ color: "white" }} />
@@ -245,6 +132,7 @@ export const Sidebar = ({ dummyList }: SidebarProps) => {
                     item.subBtn ? toggleSubmenu(item.title) : null
                   }
                   sx={{ alignItems: "center" }}
+                  {...(item.to ? { to: item.to } : undefined )}
                 >
                   <ListItemIcon>
                     {/* <InboxIcon /> */}
@@ -270,12 +158,16 @@ export const Sidebar = ({ dummyList }: SidebarProps) => {
                   >
                     <List component="div" disablePadding>
                       {item.subBtn.map((subItem, subIndex) => (
-                        <ListItem key={subIndex} button sx={{ pl: 4 }}>
+                        <ListItemButton 
+                          key={subIndex} 
+                          sx={{ pl: 4 }} 
+                          {...(subItem.to ? { to: subItem.to } : undefined )}
+                        >
                           <ListItemText
-                            primary={subItem}
+                            primary={subItem.title}
                             style={{ color: "white" }}
                           />
-                        </ListItem>
+                        </ListItemButton>
                       ))}
                     </List>
                   </Collapse>
--- client/src/pages/supplier/shipping-queue.tsx
+++ client/src/pages/supplier/shipping-queue.tsx
@@ -1,73 +1,18 @@
-import CatalogIcon from "@/assets/svgIcons/catalog.svg";
-import DashboardIcon from "@/assets/svgIcons/dashboard.svg";
-import InventoryIcon from "@/assets/svgIcons/inventory.svg";
-import OrdersIcon from "@/assets/svgIcons/orders.svg";
-import PaymentsIcon from "@/assets/svgIcons/payments.svg";
-import PerformanceIcon from "@/assets/svgIcons/performance.svg";
-import ShipmentIcon from "@/assets/svgIcons/shipment.svg";
-import { AppBarComponent } from "@/components/common/appBar";
-import { Sidebar } from "@/components/common/sideBar";
 import ShippingtableComponent from "@/components/supplierComponent/Shippingtable.Component";
 import ShippingQueueComponent from "@/components/supplierComponent/shippingqueue.component";
 import "../../components/supplierComponent/supplierStyle.scss";
+import { SupplierWrapper } from "./supplierWrapper";
 
-const dummyList = [
-    {
-        to: "/dashboard/home",
-        icon: DashboardIcon,
-        title: "Dashboard",
-    },
-    {
-        to: "/inventory",
-        icon: InventoryIcon,
-        title: "Inventory",
-        subBtn: ["All Inventory", "Shipments", "Analytics"],
-    },
-    {
-        to: "/catalog",
-        icon: CatalogIcon,
-
-        title: "Catalog",
-        subBtn: ["Add Product", "Selling Application"],
-    },
-    {
-        to: "/orders",
-        icon: OrdersIcon,
-        title: "Orders",
-        subBtn: ["Manage Orders", "Manage Returns", "Manage Claim"],
-    },
-
-    {
-        to: "/shipments",
-        icon: ShipmentIcon,
-
-        title: "Shipments",
-    },
-    {
-        to: "/performance",
-        icon: PerformanceIcon,
-
-        title: "Performance",
-        subBtn: ["Store Analytic", "Store Review"],
-    },
-    {
-        to: "/payments",
-        icon: PaymentsIcon,
-        title: "Payments",
-    },
-];
 
 const ShippingQueue = () => {
-    return (
-        <div>
-            <Sidebar dummyList={dummyList} />
-            <AppBarComponent />
-            <main>
-                <ShippingQueueComponent />
-                <ShippingtableComponent />
-            </main>
-        </div>
-    )
+  return (
+    <SupplierWrapper>
+      <>
+        <ShippingQueueComponent />
+        <ShippingtableComponent />
+      </>
+    </SupplierWrapper>
+  )
 }
 
 export default ShippingQueue;
